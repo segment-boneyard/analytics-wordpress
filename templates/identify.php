8427e7debdf9f8662c62dd9a10cd575849e19918
Justin Sainton <justinsainton@gmail.com>
2014-07-10 15:55:00 -0700

    As it won't be feasible to register a script due to how the snippet is loaded, we can't wp_localize_script().  Therefore, we'll shore up what we currently have in place.  Fixes #23

--- templates/identify.php
+++ templates/identify.php
@@ -1,3 +1,3 @@
 <script type="text/javascript">
-	analytics.identify(<?php echo '"' . $user_id . '"' ?><?php if (!empty($traits)) { echo ', ' . json_encode($traits); } else { echo ', {}'; } ?><?php if (!empty($options)) { echo ', ' . json_encode($options); } ?>);
+	analytics.identify( <?php echo '"' . intval( $user_id ) . '"' ?><?php if ( ! empty( $traits ) ) { echo ', ' . json_encode( array_map( 'esc_js', $traits ) ); } else { echo ', {}'; } ?><?php if ( ! empty( $options ) ) { echo ', ' . json_encode( array_map( 'esc_js', $options ) ); } ?>);
 </script>
